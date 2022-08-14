1d4b6a5ae203d998c5184e544095d59702a11fcb
ObodoakorDavid <obodoakordavid@outlook.com>
2022-08-13 14:07:28 -1100

    updated the deleted, created and updated pages

--- todo/urls.py
+++ todo/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, include
-from .views import todo_list, todo_detail, todo_create, todo_update, todo_delete
+from .views import todo_list, todo_detail, todo_create, todo_update, todo_delete, todo_deleted, todo_created, todo_updated
 
 urlpatterns = [
     path('', todo_list),
     path('todo_create/', todo_create),
+    path('todo_deleted/', todo_deleted),
+    path('todo_created/', todo_created),
+    path('todo_updated/', todo_updated),
     path('<id>/todo_update/', todo_update),
     path('<id>/todo_delete/', todo_delete),
     path('<id>/', todo_detail),
--- todo/views.py
+++ todo/views.py
@@ -21,7 +21,8 @@ def todo_detail(request, id):
     return render(request, 'todo/todo_detail.html', context)
 
 def todo_create(request):
-    form = Todoform(request.POST or None)
+    form = Todoform(request.POST)
+    print(request.POST)
     if form.is_valid():
         # print(form.cleaned_data)
         # name = form.cleaned_data['name']
@@ -29,7 +30,7 @@ def todo_create(request):
         # print(name, due_date)
         # new_todo = Todo.objects.create(name=name, due_date=due_date)
         form.save()
-        return redirect('/')
+        return redirect('/todo_created')
     context = {'form': form}
 
     return render(request, 'todo/todo_create.html', context)
@@ -45,7 +46,7 @@ def todo_update(request, id):
         # print(name, due_date)
         # new_todo = Todo.objects.create(name=name, due_date=due_date)
         form.save()
-        return redirect('/')
+        return redirect('/todo_updated')
     context = {'form': form}
 
     return render(request, 'todo/todo_update.html', context)
@@ -54,5 +55,14 @@ def todo_update(request, id):
 def todo_delete(request, id):
     todo = Todo.objects.get(id=id)
     todo.delete()
-    return redirect('/')
+    return redirect('/todo_deleted')
+
+def todo_deleted(request):
+    return render(request, 'todo/todo_deleted.html')
+
+def todo_created(request):
+    return render(request, 'todo/todo_created.html')
+
+def todo_updated(request):
+    return render(request, 'todo/todo_updated.html')
 
