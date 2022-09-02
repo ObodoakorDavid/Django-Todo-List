2f80d7e81ed884c57e7e277177e85eb1c9ea50c7
ObodoakorDavid <obodoakordavid@outlook.com>
2022-09-02 08:34:12 -1100

    changed allowed host

--- Tutorial/settings.py
+++ Tutorial/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-*f^$l!@3h@i5&b_=0#73yr2vhd&tobuj6g(%#@lzmh@_#iwgwx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['daviddjangotodolist.herokuapp.com/']
+ALLOWED_HOSTS = ['daviddjangotodolist.herokuapp.com']
 
 
 # Application definition
